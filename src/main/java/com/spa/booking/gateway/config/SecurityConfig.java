82894571a224beb0f08a42a6c87c240cb0ae4516
taingovan97 <taingovan97@gmail.com>
2025-12-18 00:23:27 +0700

    fix: update SecurityConfig

--- src/main/java/com/spa/booking/gateway/config/SecurityConfig.java
+++ src/main/java/com/spa/booking/gateway/config/SecurityConfig.java
@@ -1,15 +1,17 @@
 package com.spa.booking.gateway.config;
 
+import com.spa.booking.gateway.utils.KeycloakRoleConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain security(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(ex -> ex
