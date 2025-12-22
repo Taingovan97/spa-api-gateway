010b961853e2558dec3ebeaf65e678ffc9f087e1
taingovan97 <taingovan97@gmail.com>
2025-12-22 23:24:43 +0700

    fix: add missing annotation @Configuration for SecurityConfig

--- src/main/java/com/spa/booking/gateway/config/SecurityConfig.java
+++ src/main/java/com/spa/booking/gateway/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.spa.booking.gateway.config;
 
+import com.spa.booking.gateway.utils.KeycloakRoleConverter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
@@ -13,13 +16,18 @@ public class SecurityConfig {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(ex -> ex
+                        // public
                         .pathMatchers("/actuator/health", "/services/**").permitAll()
+
+                        // protected by role
                         .pathMatchers("/admin/**").hasRole("ADMIN")
                         .pathMatchers("/staff/**").hasAnyRole("STAFF", "ADMIN")
-                        .pathMatchers("/me/**").authenticated()
+                        .pathMatchers("/user/**").hasAnyRole("CUSTOMER",  "STAFF", "ADMIN")
+
+                        // everything else requires login
                         .anyExchange().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(new ReactiveJwtAuthenticationConverterAdapter(new KeycloakRoleConverter()))))
                 .build();
     }
 }
--- src/main/java/com/spa/booking/gateway/utils/KeycloakRoleConverter.java
+++ src/main/java/com/spa/booking/gateway/utils/KeycloakRoleConverter.java
@@ -1,20 +1,42 @@
 package com.spa.booking.gateway.utils;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
-public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+public class KeycloakRoleConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+    private static final Set<String> IGNORED_REALM_ROLES = Set.of(
+            "offline_access",
+            "uma_authorization"
+    );
+
     @Override
-    public Collection<GrantedAuthority> convert(Jwt jwt) {
+    public AbstractAuthenticationToken convert(Jwt jwt) {
+        JwtGrantedAuthoritiesConverter scopes = new JwtGrantedAuthoritiesConverter();
+        Collection<GrantedAuthority> scopeAuthorities = Optional.ofNullable(scopes.convert(jwt)).orElse(List.of());
+
+        // extract realm roles
+        Collection<GrantedAuthority> realmsAuthorities = extractRealmRoles(jwt);
+        List<GrantedAuthority> authorities = Stream.concat(scopeAuthorities.stream(), realmsAuthorities.stream()).toList();
+
+        System.out.println("JWT sub=" + jwt.getSubject());
+        System.out.println("realm_access.roles=" + jwt.getClaimAsMap("realm_access"));
+        System.out.println("AUTHORITIES=" + authorities);
+
+        return new JwtAuthenticationToken(jwt, authorities);
+    }
+
+    private Collection<GrantedAuthority> extractRealmRoles(Jwt jwt) {
         Map<String, Object> realmAccess = jwt.getClaimAsMap("realm_access");
         if (realmAccess == null) return List.of();
 
@@ -22,7 +44,10 @@ public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedA
         List<String> roles = (List<String>) realmAccess.getOrDefault("roles", List.of());
 
         return roles.stream()
+                .filter(Objects::nonNull)
+                .map(String::valueOf)
+                .filter(r -> !IGNORED_REALM_ROLES.contains(r))
                 .map(r -> new SimpleGrantedAuthority("ROLE_" + r))
-                .collect(Collectors.toSet());
+                .collect(Collectors.toList());
     }
 }
